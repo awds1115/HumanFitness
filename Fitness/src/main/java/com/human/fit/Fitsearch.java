ad49cfeef74abb35a78f901025c4ba96140e3bbf
admin <admin@DESKTOP-7TO09PP>
2022-03-08 09:29:50 +0900

    MHW

---
+++ Fitness/src/main/java/com/human/fit/Fitsearch.java
@@ -0,0 +1,124 @@
+package com.human.fit;
+
+public class Fitsearch {
+	private String num;
+	private String name;
+	private String kcal;
+	private String carbohydrate;
+	private String protein;
+	private String fat;
+	private String sugars;
+	private String na;
+	private String cholesterol;
+	private String saturated_fatty_acids;
+	private String transfat;
+	
+	public Fitsearch() {
+	}
+
+	public Fitsearch(String num, String name, String kcal, String carbohydrate, String protein, String fat,
+			String sugars, String na, String cholesterol, String saturated_fatty_acids, String transfat) {
+		this.num = num;
+		this.name = name;
+		this.kcal = kcal;
+		this.carbohydrate = carbohydrate;
+		this.protein = protein;
+		this.fat = fat;
+		this.sugars = sugars;
+		this.na = na;
+		this.cholesterol = cholesterol;
+		this.saturated_fatty_acids = saturated_fatty_acids;
+		this.transfat = transfat;
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getKcal() {
+		return kcal;
+	}
+
+	public void setKcal(String kcal) {
+		this.kcal = kcal;
+	}
+
+	public String getCarbohydrate() {
+		return carbohydrate;
+	}
+
+	public void setCarbohydrate(String carbohydrate) {
+		this.carbohydrate = carbohydrate;
+	}
+
+	public String getProtein() {
+		return protein;
+	}
+
+	public void setProtein(String protein) {
+		this.protein = protein;
+	}
+
+	public String getFat() {
+		return fat;
+	}
+
+	public void setFat(String fat) {
+		this.fat = fat;
+	}
+
+	public String getSugars() {
+		return sugars;
+	}
+
+	public void setSugars(String sugars) {
+		this.sugars = sugars;
+	}
+
+	public String getNa() {
+		return na;
+	}
+
+	public void setNa(String na) {
+		this.na = na;
+	}
+
+	public String getCholesterol() {
+		return cholesterol;
+	}
+
+	public void setCholesterol(String cholesterol) {
+		this.cholesterol = cholesterol;
+	}
+
+	public String getSaturated_fatty_acids() {
+		return saturated_fatty_acids;
+	}
+
+	public void setSaturated_fatty_acids(String saturated_fatty_acids) {
+		this.saturated_fatty_acids = saturated_fatty_acids;
+	}
+
+	public String getTransfat() {
+		return transfat;
+	}
+
+	public void setTransfat(String transfat) {
+		this.transfat = transfat;
+	}
+	
+	
+	
+}
---
+++ Fitness/src/main/java/com/human/fit/MHWController.java
@@ -0,0 +1,55 @@
+package com.human.fit;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+@Controller
+public class MHWController {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping("/search")
+	public String search() {
+		return "List/search";
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/searchview", produces="application/json;charset=utf-8", method=RequestMethod.GET)
+	public String searchview(HttpServletRequest hsr) {
+		String keyword=hsr.getParameter("value");
+		System.out.println(keyword);	
+		iSearch search=sqlSession.getMapper(iSearch.class);
+		ArrayList<Fitsearch> S=search.getSearchList(keyword);
+		System.out.println(S.size());
+		JSONArray ja=new JSONArray();
+		for(int i=0; i<S.size(); i++) {
+			JSONObject jo=new JSONObject();
+			jo.put("num", S.get(i).getNum());
+			jo.put("name", S.get(i).getName());
+			jo.put("kcal", S.get(i).getKcal());
+			jo.put("carbohydrate", S.get(i).getCarbohydrate());
+			jo.put("protein", S.get(i).getProtein());
+			jo.put("fat", S.get(i).getFat());
+			jo.put("sugars", S.get(i).getSugars());
+			jo.put("na", S.get(i).getNa());
+			jo.put("cholesterol", S.get(i).getCholesterol());
+			jo.put("saturated_fatty_acids", S.get(i).getSaturated_fatty_acids());
+			jo.put("transfat", S.get(i).getTransfat());
+			ja.add(jo);
+		}
+		return ja.toString();
+	}
+}
