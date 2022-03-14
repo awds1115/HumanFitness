d3aa7562d7f28e8c03008cda6debed138a763115
admin <admin@DESKTOP-7TO09PP>
2022-03-14 09:29:50 +0900

    2022.03.14. 현웅

--- Fitness/src/main/java/com/human/fit/MHWController.java
+++ Fitness/src/main/java/com/human/fit/MHWController.java
@@ -1,17 +1,17 @@
 package com.human.fit;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -21,11 +21,31 @@ public class MHWController {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	
+	
+	/* 게시판 목록 페이지 접속(페이징 적용) */
+	@RequestMapping(value = "/searchlist", method = RequestMethod.GET)
+    public String searchListGET(Model model, SearchPaging sp) {
+		iSearch search=sqlSession.getMapper(iSearch.class);
+        
+        model.addAttribute("list", search.getListPaging(sp));
+        
+        int total = search.getTotal(sp);
+        
+        SearchPageMakerDTO pageMake = new SearchPageMakerDTO(sp, total);
+        
+        model.addAttribute("pageMaker", pageMake);
+        
+        return "List/search";
+        
+    }
+	
 	@RequestMapping("/search")
 	public String search() {
+		
 		return "List/search";
 	}
-
+	
 	@ResponseBody
 	@RequestMapping(value="/searchview", produces="application/json;charset=utf-8", method=RequestMethod.GET)
 	public String searchview(HttpServletRequest hsr) {
--- Fitness/src/main/java/com/human/fit/iSearch.java
+++ Fitness/src/main/java/com/human/fit/iSearch.java
@@ -1,7 +1,21 @@
 package com.human.fit;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface iSearch {
 	ArrayList<Fitsearch> getSearchList(String keyword);
+	
+	/* 게시판 목록 */
+	public List<Fitsearch> getList();
+	
+	/* 게시판 목록(페이징 적용) */
+	public List<Fitsearch> getListPaging(SearchPaging sp);
+	
+	/* 게시판 조회 */
+	public Fitsearch getPage(int num);
+	
+	/* 게시판 총 개수 */
+	public int getTotal(SearchPaging sp);
 }
+	
